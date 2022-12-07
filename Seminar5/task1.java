195b9be73d203864adab45c40ee8efe8c7191568
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-12-07 20:15:01 +0300

    add TAsk seminar 5

---
+++ Seminar5/task1.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        String str_1 = scan.next();
+        String str_2 = scan.next();
+        Boolean flag = true;
+        if (str_1.length() != str_2.length()) {
+            flag = false;
+        } else {
+            HashMap<String, String> morph = new HashMap<>();
+            for (int i = 0; i < args.length; i++) {
+                
+            }
+        }
+    }
+}
